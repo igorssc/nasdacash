c0917f09424161fa2b5eb068f2c162debb7bf341
Igor Santos <igorsantoscosta@gmail.com>
2022-08-22 14:01:13 -0300

    Footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,13 +13,15 @@ export const Footer = () => {
   return (
     <>
       <div className="bg-black py-8">
-        <div className="container mx-auto flex flex-row justify-between">
+        <div className="container mx-auto flex flex-col md:flex-row flex-wrap justify-between items-center gap-8 md:gap-0">
           <div>
             <img src={logoImg} alt="Logo from Nasdacash" className="w-40" />
           </div>
-          <div className="flex gap-8 items-center">
-            <span>&copy; nasdacash 2020 - {new Date().getUTCFullYear()}</span>
-            <div className="flex gap-6">
+          <div className="flex gap-8 flex-col md:flex-row items-center">
+            <span className="order-2 md:order-1">
+              &copy; nasdacash 2020 - {new Date().getUTCFullYear()}
+            </span>
+            <div className="flex gap-6 order-1 md:order-2">
               <a href="https://explorer2.nasda.cash" target="_blank">
                 <Binoculars size={32} color="#ffffff" />
               </a>
